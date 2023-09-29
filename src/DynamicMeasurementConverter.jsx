98501f93b9c09aa11ad8fdc9e3f831db810617a1
Sukhvindra9718 <sukhvindra.singh@resilientitservices.com>
2023-09-29 18:39:23 +0530

    second commit

--- src/DynamicMeasurementConverter.jsx
+++ src/DynamicMeasurementConverter.jsx
@@ -5,35 +5,50 @@ export function DynamicMeasurementConverter(props) {
     const [values, setValues] = useState(0.00);
     const [conversion, setConversion] = useState(0.00);
     const [dec, setDec] = useState("");
+    let clearTimeout;
 
-    const handleChange = async (e) => {
-        setDec(e.target.value);
+
+    async function callMicroflow(eVal) {
+        console.log("callMicroflow",dec);
+        const decValue = "" + (eVal*conversion)
         // Set TextValue        
         props.inputValue.setValue(decValue);
 
 
         // Set TextValue        
         props.inputValue.setTextValue(decValue);
-        if (props.onChange && props.onChange.canExecute) {
-            props.onChange.execute();
-        }
+        setTimeout(() => {
+            // Call a microflow
+            if (props.onChange && props.onChange.canExecute) {
+                props.onChange.execute();
+            }
+        }, 200);
+    }
+    const handleChange = (e) => {
+        console.log("handleChange");
+        setDec(e.target.value);
+        console.log("e.ta",e.target.value);
+        clearTimeout = setTimeout(() => {
+            callMicroflow(e.target.value);
+        }, 500);
+        // callMicroflow();
     };
     const handleFocus = (event) => event.target.select();
 
     const handleFocusOut = () => {
-        const decValue = "" + values
+        // const decValue = "" + values
 
         // Set TextValue        
-        props.inputValue.setValue(decValue);
+        // props.inputValue.setValue(decValue);
 
 
-        // Set TextValue        
-        props.inputValue.setTextValue(decValue);
+        // // Set TextValue        
+        // props.inputValue.setTextValue(decValue);
 
-        // Call a microflow
-        if (props.onChange && props.onChange.canExecute) {
-            props.onChange.execute();
-        }
+        // // Call a microflow
+        // if (props.onChange && props.onChange.canExecute) {
+        //     props.onChange.execute();
+        // }
     }
 
     useEffect(() => {
@@ -50,7 +65,7 @@ export function DynamicMeasurementConverter(props) {
     return (
         <Fragment>
             <div className="textbox">
-                <p>Convert value</p>
+                <p>{props.labelcaption}</p>
                 <input
                     type="text"
                     value={dec}
