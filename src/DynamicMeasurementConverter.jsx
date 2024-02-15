b31b838108d70ca37473174e5613c5a3154a36fd
Sukhvindra9718 <sukhvindra.singh@resilientitservices.com>
2024-02-15 19:46:45 +0530

    15.2.24

--- src/DynamicMeasurementConverter.jsx
+++ src/DynamicMeasurementConverter.jsx
@@ -1,83 +1,89 @@
 import { createElement, Fragment, useEffect, useState } from "react";
 import "./ui/DynamicMeasurementConverter.css";
+import classNames from "classnames";
 
 export function DynamicMeasurementConverter(props) {
-    const [values, setValues] = useState(0.00);
-    const [conversion, setConversion] = useState(0.00);
+    const [values, setValues] = useState(0.0);
+    const [conversion, setConversion] = useState(0.0);
     const [dec, setDec] = useState("");
-    let clearTimeout;
 
 
     async function callMicroflow(eVal) {
-        const decValue = "" + (eVal*conversion)
-        // Set TextValue        
-        props.inputValue.setValue(decValue);
+        const decValue = "" + eVal * conversion;
 
-
-        // Set TextValue        
-        props.inputValue.setTextValue(decValue);
-        setTimeout(() => {
-            // Call a microflow
-            if (props.onChange && props.onChange.canExecute) {
+        // Set TextValue
+        props.inputValue.setTextValue(parseFloat(decValue).toFixed(6));
+        if (props.onChange && props.onChange.canExecute) {
+            setTimeout(() => {
                 props.onChange.execute();
-            }
-        }, 200);
+            }, 300);
+        }
     }
-    const handleChange = (e) => {
+    const handleChange = e => {
         setDec(e.target.value);
-        clearTimeout = setTimeout(() => {
+        setTimeout(() => {
             callMicroflow(e.target.value);
-        }, 500);
+            clearTimeout();
+        }, 3000);
     };
-    const handleFocus = (event) => event.target.select();
+    const handleFocus = event => event.target.select();
 
     const handleFocusOut = () => {
-        const decValue = "" + values
-        console.log("decValue", decValue);
-        // Set TextValue      
-        props.inputValue.setValue(decValue);
+        const decValue = "" + values;
 
-
-        // Set TextValue        
-        props.inputValue.setTextValue(decValue);
-
+        // Set TextValue
+        props.inputValue.setTextValue(parseFloat(decValue).toFixed(6));
         // Call a microflow
-        if (props.onChange && props.onChange.canExecute) {
-            props.onChange.execute();
+        if (props.onBlurChange && props.onBlurChange.canExecute) {
+            props.onBlurChange.execute();
         }
+    };
+    const calculateUnit = () => {
+        setConversion(parseFloat(props.inputConversionRate.displayValue));
+        setValues(parseFloat(props.inputValue.displayValue));
+
+        var ans =
+            isNaN(values) && isNaN(conversion)
+                ? "0"
+                : props.inputValue.displayValue / props.inputConversionRate.displayValue;
+
+
+        var precise = parseFloat(ans).toFixed(props.decimalPrecision.value);
+        setDec(precise);
     }
 
     useEffect(() => {
-        setConversion(parseFloat(props.inputConversionRate.displayValue));
-        setValues(parseFloat(props.inputValue.displayValue));
-        var ans = (isNaN(values) && isNaN(conversion)) ? "0" : parseFloat(props.inputValue.displayValue) / parseFloat(props.inputConversionRate.displayValue);
-     
-        var precise = parseFloat(ans).toFixed(props.decimalPrecision.value);
-        setDec(precise);
+        calculateUnit();
+        return () => { }
     }, [props.inputConversionRate, props.inputValue]);
 
     useEffect(() => {
-        setValues(dec * conversion);
+        return () => setValues(dec * conversion);
     }, [dec]);
 
     return (
         <Fragment>
-            <div style={props.style} className={props.class}>
+            <div style={props.style} className={classNames(props.class, 'form-group', 'dynamicMeasurementConverterInput')}>
                 <input
                     type="text"
-                    value={dec}
+                    value={isNaN(dec) ? " ": dec}
+                    placeholder={props.placeholder}
                     onFocus={e => {
                         handleFocus(e);
                     }}
                     onBlur={e => {
                         handleFocusOut(e);
                     }}
-                    onChange={(e) => handleChange(e)}
-                    onKeyDown={(evt) => ["e", "E", "+", "-", "ArrowDown", "ArrowUp"].includes(evt.key) && evt.preventDefault()}
-                    style={{ width: "100%" }}
+                    onChange={e => handleChange(e)}
+                    onKeyDown={evt =>
+                        ["e", "E", "+", "-", "ArrowDown", "ArrowUp"].includes(evt.key) && evt.preventDefault()
+                    }
+                    className="form-control"
+                    autoComplete="on"
+                    disabled={props?.inputValue?.readOnly}
                 />
+                <span className="unitLabel">{props.unit}</span>
             </div>
         </Fragment>
-
     );
 }
